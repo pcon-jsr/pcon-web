3a1ba4873abaa30539b09d3f4a457bcd07575ea8
Aditya Pratap <aditya@buyhatke.com>
2020-07-16 16:03:47 +0530

    updated to use browser router, updated content

--- src/components/message-box/message-box.component.jsx
+++ src/components/message-box/message-box.component.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './message-box.styles.css';
-import firebase from  "./data.js";
+import firebase from "./data.js";
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Snackbar from '@material-ui/core/Snackbar';
@@ -16,12 +16,12 @@ const useStyles = makeStyles(theme => ({
     textField: {
 
         width: 250,
-        height:'90%'
+        height: '90%'
 
     },
-    button:{
-        width:80,
-        display:'flex',
+    button: {
+        width: 80,
+        display: 'flex',
         justifyContent: 'space-around'
     }
 }));
@@ -30,19 +30,19 @@ const useStyles = makeStyles(theme => ({
 
 function TransitionLeft(props) {
     return <Slide {...props} direction="left" />;
-  }
-  
-  function TransitionUp(props) {
+}
+
+function TransitionUp(props) {
     return <Slide {...props} direction="up" />;
-  }
-  
-  function TransitionRight(props) {
+}
+
+function TransitionRight(props) {
     return <Slide {...props} direction="right" />;
-  }
-  
-  function TransitionDown(props) {
+}
+
+function TransitionDown(props) {
     return <Slide {...props} direction="down" />;
-  }
+}
 
 
 
@@ -56,116 +56,115 @@ function TransitionLeft(props) {
 
 const MessageBox = () => {
     const classes = useStyles();
-    
-let [msg,setMsg]=useState({
-    email:'',
-    message:'',
-    name:'',
-});
-   
-const [open, setOpen] = React.useState(false);
-  const [transition, setTransition] = React.useState(undefined); 
+
+    let [msg, setMsg] = useState({
+        email: '',
+        message: '',
+        name: '',
+    });
+
+    const [open, setOpen] = React.useState(false);
+    const [transition, setTransition] = React.useState(undefined);
 
 
 
 
-const handleClick = Transition => () => {
-    setTransition(() => Transition);
-    setOpen(true);
-  };
+    const handleClick = Transition => () => {
+        setTransition(() => Transition);
+        setOpen(true);
+    };
 
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-
+    const handleClose = () => {
+        setOpen(false);
+    };
 
 
 
 
 
-const mySubmitHandler=(event)=>{
-    event.preventDefault();
-}
+
+
+    const mySubmitHandler = (event) => {
+        event.preventDefault();
+    }
 
 
 
-const nameHandler=(event)=>{
-          
+    const nameHandler = (event) => {
+
         setMsg({
             ...msg,
-            name:event.target.value
+            name: event.target.value
         })
-}
+    }
 
-const emailHandler=(event)=>{
-    setMsg({
-        ...msg,
-        email:event.target.value
-    })
-}
+    const emailHandler = (event) => {
+        setMsg({
+            ...msg,
+            email: event.target.value
+        })
+    }
 
-const msgHandler=(event)=>{
-    setMsg({
-        ...msg,
-        message:event.target.value
-    })
-}
+    const msgHandler = (event) => {
+        setMsg({
+            ...msg,
+            message: event.target.value
+        })
+    }
 
 
-const check=()=>{
-    //let numbers = /^[0-9]+$/;
-    let  regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const check = () => {
+        //let numbers = /^[0-9]+$/;
+        let regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    if(msg.email==="" || !regEx.test(String(msg.email).toLowerCase()) )
-        return false;
-        if(msg.name==="")
-           return false;    
-           if(msg.message==="")
-        return false;
+        if (msg.email === "" || !regEx.test(String(msg.email).toLowerCase()))
+            return false;
+        if (msg.name === "")
+            return false;
+        if (msg.message === "")
+            return false;
 
         return true;
-}
+    }
 
 
 
-const pushDetails=()=>{
-    var d = new Date();
-    var n = d.toString();
-      console.log(msg);
-      let x={
-          ...msg,
-          time:n
-      }
-     
+    const pushDetails = () => {
+        var d = new Date();
+        var n = d.toString();
+        console.log(msg);
+        let x = {
+            ...msg,
+            time: n
+        }
 
-     if(check()){
-    firebase.database().ref("/messages").push(
-       x
-    )
-    .then((res)=>{
-        //   alert("successfully added");
-        handleClick(TransitionLeft)();
-          console.log( "response",res );
-          setMsg({
-            ...msg,  
-           email:"",
-           name:"",
-           message:""
-          })
-    });
-}
-else
-{
-    alert("please enter the details correctly");
-}
-    
-    // .once('value',(snap)=> {
-    //     console.log(snap.val());
-    // });
-   
-   }
-   
+
+        if (check()) {
+            firebase.database().ref("/messages").push(
+                x
+            )
+                .then((res) => {
+                    //   alert("successfully added");
+                    handleClick(TransitionLeft)();
+                    console.log("response", res);
+                    setMsg({
+                        ...msg,
+                        email: "",
+                        name: "",
+                        message: ""
+                    })
+                });
+        }
+        else {
+            alert("please enter the details correctly");
+        }
+
+        // .once('value',(snap)=> {
+        //     console.log(snap.val());
+        // });
+
+    }
+
 
     return (
         <div className='message-box'>
@@ -173,10 +172,12 @@ else
                 <p>MESSAGE US</p>
             </div>
 
-            <form className='body'   onSubmit={mySubmitHandler}   noValidate autoComplete="off">
+            <form className='body' onSubmit={mySubmitHandler} noValidate autoComplete="off">
 
-                <div style={{height:'20%',display:'flex',
-        justifyContent: 'space-around'}}>
+                <div style={{
+                    height: '20%', display: 'flex',
+                    justifyContent: 'space-around'
+                }}>
                     <TextField
                         id="name"
                         className={classes.textField}
@@ -185,12 +186,14 @@ else
                         margin="normal"
                         variant="outlined"
                         onChange={nameHandler}
-                       
+
                         value={msg.name}
                     />
                 </div>
-                <div  style={{height:'20%',display:'flex',
-        justifyContent: 'space-around'}}>
+                <div style={{
+                    height: '20%', display: 'flex',
+                    justifyContent: 'space-around'
+                }}>
                     <TextField
                         id="email"
                         className={classes.textField}
@@ -198,20 +201,22 @@ else
                         type="email"
                         margin="normal"
                         variant="outlined"
-                       
+
                         onChange={emailHandler}
                         value={msg.email}
                     />
                 </div>
-                <div  style={{height:'40%',display:'flex',
-        justifyContent: 'space-around'}}>
+                <div style={{
+                    height: '40%', display: 'flex',
+                    justifyContent: 'space-around'
+                }}>
                     <TextField
                         id="messsage"
                         label="Message"
                         multiline
                         rows="6"
                         onChange={msgHandler}
-                       
+
                         className={classes.textField}
                         margin="normal"
                         variant="outlined"
@@ -222,27 +227,27 @@ else
                     variant="contained"
                     onClick={pushDetails}
                     className={classes.button}
-                   style={{width:'110px', marginTop:'25px', background:'#038A86', color:'#fff'}}
+                    style={{ width: '110px', marginTop: '25px', background: '#038A86', color: '#fff' }}
                 >
                     Send
-                    <SendIcon style={{width:'20px'}}/>
+                    <SendIcon style={{ width: '20px' }} />
                 </Button>
             </form>
             <div>
-      {/* <Button onClick={handleClick(TransitionLeft)}>Right</Button> */}
-      
-      <Snackbar
-        open={open}
-        onClose={handleClose}
-        TransitionComponent={transition}
-        classes={{color: "white",backgroundColor:"green"}}
-        ContentProps={{
-          'aria-describedby': 'message-id'
-         
-        }}
-        message={<span id="message-id">successfully added</span>}
-      />
-    </div>
+                {/* <Button onClick={handleClick(TransitionLeft)}>Right</Button> */}
+
+                <Snackbar
+                    open={open}
+                    onClose={handleClose}
+                    TransitionComponent={transition}
+                    classes={{ color: "white", backgroundColor: "green" }}
+                    ContentProps={{
+                        'aria-describedby': 'message-id'
+
+                    }}
+                    message={<span id="message-id">successfully added</span>}
+                />
+            </div>
         </div>
     )
 }
--- src/pages/home/home.page.jsx
+++ src/pages/home/home.page.jsx
@@ -1,49 +1,81 @@
-import React from 'react';
+import React from "react";
 
-import './home.styles.css';
-import HomePageHeader from '../../components/homePageHeader/homePageHeader.component';
+import "./home.styles.css";
+import HomePageHeader from "../../components/homePageHeader/homePageHeader.component";
 // import HomePageAbout from '../../components/homePageAbout/homePageAbout.component';
 // import HomePageRecentActivities from '../../components/homePageRecentActivities/homePageRecentActivities.component';
 // import OtherPagesSection from '../../components/otherPagesSection/otherPagesSection.component';
-import RectangularContainer from '../../components/rectangular-container/rectangular-container.component';
-import HomePageSection from '../../components/homePageSection/homePageSection.component';
-
-
+import RectangularContainer from "../../components/rectangular-container/rectangular-container.component";
+import HomePageSection from "../../components/homePageSection/homePageSection.component";
 
 class HomePage extends React.Component {
+  changeRoute = (text) => {
+    this.props.history.push(`/${text}`);
+  };
 
-    changeRoute = (text) => {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
 
-        this.props.history.push(`/${text}`);
-    }
-
-    componentDidMount() {
-        window.scrollTo(0, 0)
-    }
-
-    render() {
-        console.log('Home:', this.props)
-        return (
-            <div className='home'>
-                <HomePageHeader />
-                <RectangularContainer>
-                    <HomePageSection>
-                        <h2>Who Are We?</h2>
-                    </HomePageSection>
-                    <HomePageSection grey>
-                        <h2>About the Institute</h2>
-                    </HomePageSection>
-                    <HomePageSection>
-                        <h2>Highlights</h2>
-                    </HomePageSection>
-                    {/* <HomePageAbout />
+  render() {
+    console.log("Home:", this.props);
+    return (
+      <div className="home">
+        <HomePageHeader />
+        <RectangularContainer>
+          <HomePageSection>
+            <h2>Who Are We?</h2>
+            <p>
+              We, at Programming Club of NIT Jamshedpur, are a group of highly
+              enthusiastic and dedicated individuals striving to spread
+              algorithmic thinking to ignite the minds of the contemporary
+              generation to Code for the Future. PCON consists of a diverse
+              group which focuses on Competitive Coding, App and web design,
+              Cloud Computing, Machine Learning and many more. If you think that
+              the bits are the alphabets of the future, you are One of Us,
+              spreading the gene-o-code all the way!
+            </p>
+          </HomePageSection>
+          <HomePageSection grey>
+            <h2>About the Institute</h2>
+            <p>
+              National Institute of Technology, Jamshedpur,earlier known as
+              Regional Institute of Technology was established on 15th August
+              1960 as a joint venture of Government of India and the Government
+              of Bihar in the chain of REC's (Regional Engineering College) in
+              India with the aim to generate technical graduates of highest
+              standards who could provide technological leadership to the
+              region. It was among the first eight Regional Engineering Colleges
+              (RECs) established as part of the Second Five-Year Plan (1956 -
+              1961). This was the only REC in the country which was named as RIT
+              (Regional Institute of Technology). Therefore RIT Jamshedpur was
+              actually the REC of undivided Bihar & Jharkhand. The foundation
+              stone of RIT (REC) Jamshedpur was laid by Dr. Srikrishna Sinha,
+              the then chief minister of Bihar , with the aim of nurturing
+              talent and setting high standards of education and excellence. 
+              <br/>
+              <br/>
+              On 27th December 2002, in the line of all other RECs of India, RIT
+              Jamshedpur (may be read as REC Jamshedpur too) was converted to
+              National Institute of Technology, Jamshedpur with the status of a
+              Deemed University as per the decision of Govt. of India. The
+              Institute is fully funded and governed by the Ministry of Human
+              Resource Development (MHRD), Government of India since 1st April,
+              2003. On 15 August 2007, NIT Jamshedpur was given the status of
+              the Institute of National Importance through an Act of the
+              Parliament known as the NIT Act.
+            </p>
+          </HomePageSection>
+          <HomePageSection>
+            <h2>Highlights</h2>
+          </HomePageSection>
+          {/* <HomePageAbout />
                     <HomePageRecentActivities />
                     <OtherPagesSection changeRoute={this.changeRoute} /> */}
-                </RectangularContainer>
-
-            </div>
-        )
-    }
+        </RectangularContainer>
+      </div>
+    );
+  }
 }
 
 export default HomePage;
